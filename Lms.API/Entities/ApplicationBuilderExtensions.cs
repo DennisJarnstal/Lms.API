5d024e02214dddf1317b637e21a2c15a50a48caa
Dennis Järnstål <dennis_jarnstal@hotmail.com>
2022-09-01 00:30:34 +0200

    13 and part of 14

---
+++ Lms.API/Entities/ApplicationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Lms.Data.Data;
+
+namespace Lms.API.Entities
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task<IApplicationBuilder>SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var db = serviceProvider.GetRequiredService<LmsAPIContext>();
+
+                var config = serviceProvider.GetRequiredService<IConfiguration>();
+
+                try
+                {
+                    await SeedData.InitAsync(db, serviceProvider);
+                }
+                catch (Exception e)
+                {
+
+                    throw;
+                }
+            }
+
+            return app;
+        }
+    }
+}
---
+++ Lms.Data/Data/SeedData.cs
@@ -0,0 +1,39 @@
+﻿using Lms.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Data
+{
+    public class SeedData
+    {
+        private static LmsAPIContext db = default!;
+        public static async Task InitAsync(LmsAPIContext context, IServiceProvider services)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            db = context;
+
+            ArgumentNullException.ThrowIfNull(nameof(services));
+
+            if (db.Course.Any()) return;
+
+            var courses = GetCourses();
+            await db.AddRangeAsync(courses);
+
+            var modules = GetModules();
+            await db.AddRangeAsync(modules);
+        }
+
+        private static IEnumerable<Course> GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Module> GetModules()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
