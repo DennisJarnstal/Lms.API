a5bb0844bf14dee7ac01bdbd4e4f3f6a8016f29f
Dennis Järnstål <dennis_jarnstal@hotmail.com>
2022-09-05 10:36:44 +0200

    17 and 18

---
+++ Lms.Data/Repositories/CourseRepository - Copy.cs
@@ -0,0 +1,50 @@
+﻿using Lms.Core.Entities;
+using Lms.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Repositories
+{
+    internal class ModuleRepository : IModuleRepository
+    {
+        private readonly IModuleRepository _moduleRepository;
+
+        public void Add(Module module)
+        {
+            _moduleRepository.Add(module);
+        }
+
+        public Task<bool> AnyAsync(int? id)
+        {
+            return _moduleRepository.AnyAsync(id);
+        }
+
+        public Task<Module> FindAsync(int? id)
+        {
+            return _moduleRepository.FindAsync(id);
+        }
+
+        public Task<IEnumerable<Module>> GetAllModules()
+        {
+            return _moduleRepository.GetAllModules();
+        }
+
+        public Task<Module> GetModule(int? id)
+        {
+            return _moduleRepository.GetModule(id);
+        }
+
+        public void Remove(Module module)
+        {
+            _moduleRepository.Remove(module);
+        }
+
+        public void Update(Module module)
+        {
+            _moduleRepository.Update(module);
+        }
+    }
+}
---
+++ Lms.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,50 @@
+﻿using Lms.Core.Entities;
+using Lms.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Repositories
+{
+    internal class CourseRepository : ICourseRepository
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public void Add(Course course)
+        {
+            _courseRepository.Add(course);
+        }
+
+        public Task<bool> AnyAsync(int? id)
+        {
+            return _courseRepository.AnyAsync(id);
+        }
+
+        public Task<Course> FindAsync(int? id)
+        {
+            return _courseRepository.FindAsync(id);
+        }
+
+        public Task<IEnumerable<Course>> GetAllCourses()
+        {
+            return _courseRepository.GetAllCourses();
+        }
+
+        public Task<Course> GetCourse(int? id)
+        {
+            return _courseRepository.GetCourse(id);
+        }
+
+        public void Remove(Course course)
+        {
+            _courseRepository.Remove(course);
+        }
+
+        public void Update(Course course)
+        {
+            _courseRepository.Update(course);
+        }
+    }
+}
