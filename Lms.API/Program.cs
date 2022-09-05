11e31600aff2a152e8562b76f7a9153158cd96a8
Dennis Järnstål <dennis_jarnstal@hotmail.com>
2022-09-05 12:17:22 +0200

    19-21

--- Lms.API/Program.cs
+++ Lms.API/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Lms.Data.Data;
 using Lms.API.Entities;
+using Lms.Core.Repositories;
+using Lms.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<LmsAPIContext>(options =>
@@ -13,6 +15,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUoW, UoW>();
 
 var app = builder.Build();
 
---
+++ Lms.Data/Repositories/UoW.cs
@@ -0,0 +1,22 @@
+﻿using Lms.Core.Repositories;
+using Lms.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Repositories
+{
+    public class UoW : IUoW
+    {
+        public ICourseRepository CourseRepository => throw new NotImplementedException();
+
+        public IModuleRepository ModuleRepository => throw new NotImplementedException();
+
+        public Task CompleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
